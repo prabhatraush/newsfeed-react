74b4d3c4eb7c045b8a3210096d621568fdaca942
Prabhat Raushan <65757931+prabhatraush@users.noreply.github.com>
2021-07-15 20:03:32 +0530

    Update FeedBanner.js

--- src/components/FeedBanner.js
+++ src/components/FeedBanner.js
@@ -100,7 +100,7 @@ function FeedBanner(props){
                 <h2>{feed.title}</h2>
             </div>
             <div className="description">
-               {feed.contentSnippet.replace( /(<([^>]+)>)/ig, '').substring(0, 1500)} . .  
+               {feed.contentSnippet.replace( /(<([^>]+)>)/ig, '').substring(0, 900)} . .  
                <a key={feed.guid} rel="noopener noreferrer" target="_blank" href={feed.link} >और पढ़ें</a>
             </div>
         </div>
